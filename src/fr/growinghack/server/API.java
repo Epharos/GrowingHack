bd109d610be65a1d6997fcc9ab7365a90e3d767e
Epharos <Neox30650@gmail.com>
2016-09-18 13:19:07 +0200

    Modification de la gestion des contacts

--- src/fr/growinghack/server/API.java
+++ src/fr/growinghack/server/API.java
@@ -107,6 +107,48 @@ public class API
 		return contacts;
 	}
 	
+	public static List<String> getPlayerContacts(int connexionId)
+	{
+		File file = null;
+		
+		for(ConnectedPlayer cp : GrowingHack.instance.server.connected)
+		{
+			if(cp.id == connexionId)
+			{
+				file = getPlayerFile(cp.username, "contactslist");
+			}
+		}
+		
+		List<String> contacts = new ArrayList<String>();
+		
+		try
+		{
+			InputStream is = new FileInputStream(file);
+			
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String line = "";
+			
+			try 
+			{
+				while((line = br.readLine()) != null)
+				{
+					contacts.add(line);
+				}
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return contacts;
+	}
+	
 	public static boolean isPlayerConnected(String username)
 	{
 		for(ConnectedPlayer cp : GrowingHack.instance.server.connected)
